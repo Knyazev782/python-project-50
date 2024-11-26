e86d234fb25332513cb653c84d7352f328098336
Alexandr Knyazev <a.knyazev944@gmail.com>
2024-11-26 13:18:03 +0300

    исправил тесты, поскольку после их запуска удалялись json файлы

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,34 +1,38 @@
 from gendiff.scripts.gendiff import generate_diff
+import pytest
 
 
-def write_temp_file(file_name, content):
-    with open(file_name, 'w') as f:
-        f.write(content)
-
-
-def test_gendiff_equal_files():
-    write_temp_file("file1.json", '{"key": "value"}')
-    write_temp_file("file2.json", '{"key": "value"}')
+def test_gendiff_equal_files(tmp_path):
+    file1 = tmp_path / "file1.json"
+    file2 = tmp_path / "file2.json"
+    file1.write_text('{"key": "value"}')
+    file2.write_text('{"key": "value"}')
     expected_result = "{\n    key: value\n}"
-    assert generate_diff("file1.json", "file2.json") == expected_result
+    assert generate_diff(str(file1), str(file2)) == expected_result
 
 
-def test_gendiff_different_files():
-    write_temp_file("file1.json", '{"key": "value1"}')
-    write_temp_file("file2.json", '{"key": "value2"}')
+def test_gendiff_different_files(tmp_path):
+    file1 = tmp_path / "file1.json"
+    file2 = tmp_path / "file2.json"
+    file1.write_text('{"key": "value1"}')
+    file2.write_text('{"key": "value2"}')
     expected_result = "{\n  - key: value1\n  + key: value2\n}"
-    assert generate_diff("file1.json", "file2.json") == expected_result
+    assert generate_diff(str(file1), str(file2)) == expected_result
 
 
-def test_gendiff_one_empty_file():
-    write_temp_file("file1.json", '{"key": "value1"}')
-    write_temp_file("file2.json", '{}')
+def test_gendiff_one_empty_file(tmp_path):
+    file1 = tmp_path / "file1.json"
+    file2 = tmp_path / "file2.json"
+    file1.write_text('{"key": "value1"}')
+    file2.write_text('{}')
     expected_result = "{\n  - key: value1\n}"
-    assert generate_diff("file1.json", "file2.json") == expected_result
+    assert generate_diff(str(file1), str(file2)) == expected_result
 
 
-def test_gendiff_both_empty_files():
-    write_temp_file("file1.json", '{}')
-    write_temp_file("file2.json", '{}')
+def test_gendiff_both_empty_files(tmp_path):
+    file1 = tmp_path / "file1.json"
+    file2 = tmp_path / "file2.json"
+    file1.write_text("{}")
+    file2.write_text("{}")
     expected_result = "{}"
-    assert generate_diff("file1.json", "file2.json") == expected_result
+    assert generate_diff(str(file1), str(file2)) == expected_result
