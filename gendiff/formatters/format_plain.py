ece7787f8e7d0b4a6b611fbb06b962660c51968b
Alexandr Knyazev <a.knyazev944@gmail.com>
2025-01-12 14:45:51 +0300

    переписал функцию работы формата plain 3

--- gendiff/formatters/format_plain.py
+++ gendiff/formatters/format_plain.py
@@ -16,7 +16,7 @@ def format_plain(diff, parent=''):
             return format_changed(path, node['old_value'], node['new_value'])
         elif status == NESTED:
             return format_nested(node['children'], path)
-        return
+        return None
 
     def format_added(path, value):
         formatted_value = format_value(value)
@@ -28,7 +28,10 @@ def format_plain(diff, parent=''):
     def format_changed(path, old_value, new_value):
         formatted_old = format_value(old_value)
         formatted_new = format_value(new_value)
-        return f"Property '{path}' was updated. From {formatted_old} to {formatted_new}"
+        return (
+            f"Property '{path}' was updated. "
+            f"From {formatted_old} to {formatted_new}"
+        )
 
     def format_nested(children, path):
         return format_plain(children, path)
@@ -38,12 +41,16 @@ def format_plain(diff, parent=''):
         for node in diff:
             key = node['key']
             path = f"{parent}.{key}" if parent else key
-            lines.append(process_node(node, path))
+            line = process_node(node, path)
+            if line:
+                lines.append(line)
     else:
         for key, node in diff.items():
             path = f"{parent}.{key}" if parent else key
-            lines.append(process_node(node, path))
-    return '\n'.join(filter(None, lines))
+            line = process_node(node, path)
+            if line:
+                lines.append(line)
+    return '\n'.join(lines)
 
 
 def format_value(value):
